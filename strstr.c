6e8fdcbadb2b915453e400399b5ddb7c58a8d331
Dmitrii Chernyshov <ragnarzone@gmail.com>
2022-02-11 12:14:29 +0100

    Extra exercises

---
+++ strstr.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	char s0[] = "dysfunctional";
+	char s1[] = "fun";
+	if (strstr(s0, s1))
+		puts("I found the fun in dysfunctional");
+	return 0;
+}
