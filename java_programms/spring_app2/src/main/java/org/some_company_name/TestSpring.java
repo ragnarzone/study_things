5ea0b7d19180162a164e279445a5956d727e5ada
Dmitrii Chernyshov <ragnarzone@gmail.com>
2022-02-19 18:03:31 +0100

    added spring_app2

---
+++ java_programms/spring_app2/src/main/java/org/some_company_name/TestSpring.java
@@ -0,0 +1,17 @@
+package org.some_company_name;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+
+        TestBean testBean = context.getBean("testBean", TestBean.class);
+
+        System.out.println(testBean.getName());
+
+        context.close();
+    }
+}
