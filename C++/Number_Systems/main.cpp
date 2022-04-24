d13ceab8e929f07cb5163e3379940d4923c1e43d
Dmitrii Chernyshov <ragnarzone@gmail.com>
2022-04-24 22:36:39 +0200

    added Number_Systems exercise

---
+++ C++/Number_Systems/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+    
+    int number1 = 15; // Decimal
+    int number2 = 017; // Octal
+    int number3 = 0x0f; // Hexadecimal
+    int number4 = 0b00001111; // Binary
+
+    std::cout << "number 1: " << number1 << std::endl;
+    std::cout << "number 2: " << number2 << std::endl;
+    std::cout << "number 3: " << number3 << std::endl;
+    std::cout << "number 4: " << number4 << std::endl;
+    return 0;
+}
